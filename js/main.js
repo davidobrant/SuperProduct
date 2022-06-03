5503c61b1ed0b54b108d6246a7b01088801c81ac
davidobrant <david.obrant@gmail.com>
2022-06-03 15:02:07 +0200

    seo

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ import { products } from '../data/products.js';
 import { createCard } from './createCard.js';
 import { renderWishList, updatePrice } from './wishList.js';
 
-
-
 /* ========== ON LOAD ========== */
 window.onload = () => {
     products.forEach(product => {
